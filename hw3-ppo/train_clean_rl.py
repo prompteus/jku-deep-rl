95a7c64a5026300da8a8c5fb2fb606c2c177ea75
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2023-07-16 11:07:12 +0200

    extract data generation into separate class in clean RL ppo

--- hw3-ppo/train_clean_rl.py
+++ hw3-ppo/train_clean_rl.py
@@ -1,4 +1,5 @@
 # docs and experiment results can be found at https://docs.cleanrl.dev/rl-algorithms/ppo/#ppopy
+from __future__ import annotations
 import argparse
 import os
 import random
@@ -40,7 +41,7 @@ def parse_args():
     # Algorithm specific arguments
     parser.add_argument("--env-id", type=str, default="CartPole-v1",
         help="the id of the environment")
-    parser.add_argument("--total-timesteps", type=int, default=500000,
+    parser.add_argument("--total-timesteps", type=int, default=5000000,
         help="total timesteps of the experiments")
     parser.add_argument("--learning-rate", type=float, default=2.5e-4,
         help="the learning rate of the optimizer")
@@ -50,7 +51,7 @@ def parse_args():
         help="total buffer size before updating")
     parser.add_argument("--gamma", type=float, default=0.99,
         help="the discount factor gamma")
-    parser.add_argument("--gae-lambda", type=float, default=0.0, #default=0.5,
+    parser.add_argument("--gae-lambda", type=float, default=0.75,
         help="the lambda for the general advantage estimation")
     parser.add_argument("--batch-size", type=int, default=64,
         help="the size of minibatches")
@@ -176,6 +177,102 @@ class PPO:
         return advantage
 
 
+class DataGenerator:
+    def __init__(self, envs: gym.Env, agent: Agent, args) -> None:
+        self.agent = agent
+        self.args = args
+        self.envs = envs
+
+    def __iter__(self) -> Iterator[tuple[int, Transition]]:
+        envs = self.envs
+        # ALGO Logic: Storage setup
+        states = torch.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape).to(device)
+        actions = torch.zeros((args.num_steps, args.num_envs) + envs.single_action_space.shape).to(device)
+        logprobs = torch.zeros((args.num_steps, args.num_envs)).to(device)
+        rewards = torch.zeros((args.num_steps, args.num_envs)).to(device)
+        dones = torch.zeros((args.num_steps, args.num_envs)).to(device)
+        values = torch.zeros((args.num_steps, args.num_envs)).to(device)
+
+        # TRY NOT TO MODIFY: start the game
+        global_step = 0
+        next_obs, _ = envs.reset()
+        next_obs = torch.Tensor(next_obs).to(device)
+        next_done = torch.zeros(args.num_envs).to(device)
+
+        while True:
+            for step in range(0, args.num_steps):
+                global_step += args.num_envs
+                states[step] = next_obs
+                dones[step] = next_done
+
+                # ALGO LOGIC: action logic
+                with torch.no_grad():
+                    action, logprob, _, value = agent.get_action_and_value(next_obs)
+                    values[step] = value.flatten()
+                actions[step] = action
+                logprobs[step] = logprob
+
+                # TRY NOT TO MODIFY: execute the game and log data.
+                next_obs, reward, terminated, truncated, infos = envs.step(action.cpu().numpy())
+                done = terminated | truncated
+                rewards[step] = torch.tensor(reward).to(device).view(-1)
+                next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(done).to(device)
+
+                # Only print when at least 1 env is done
+                if "final_info" not in infos:
+                    continue
+
+                for info in infos["final_info"]:
+                    # Skip the envs that are not done
+                    if info is None:
+                        continue
+                    print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
+                    writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
+                    writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
+
+            # bootstrap value if not done
+            with torch.no_grad():
+                next_value = agent.get_value(next_obs).reshape(1, -1)
+                next_values = torch.cat([values[1:], next_value], dim=0)
+                next_dones = torch.cat([dones[1:], next_done.reshape(1, -1)], dim=0)
+                td_targets = rewards + args.gamma * (1 - next_dones) * next_values
+                td_advantage = td_targets - values
+                gae_advantages = torch.zeros_like(rewards).to(device)
+                gae_advantages = torch.cat([gae_advantages, torch.zeros_like(gae_advantages[:1])], dim=0)
+                for t in reversed(range(args.num_steps)):
+                    gae_advantages[t] = td_advantage[t] + args.gamma * args.gae_lambda * (1-next_dones[t]) * gae_advantages[t+1]
+                gae_advantages = gae_advantages[:-1]
+                gae_returns = gae_advantages + values
+
+
+            # flatten the batch
+            b_states = states.reshape((-1,) + envs.single_observation_space.shape)
+            b_values = values.flatten()
+            b_actions = actions.reshape((-1,) + envs.single_action_space.shape)
+            b_rewards = rewards.flatten()
+            b_log_probs = logprobs.flatten()
+            b_gae_advantages = gae_advantages.flatten()
+            b_gae_returns = gae_returns.flatten()
+
+            # Optimizing the policy and value network
+            b_inds = np.arange(args.buffer_size)
+
+            for _ in range(args.buffer_repeat):
+                np.random.shuffle(b_inds)
+                for start in range(0, args.buffer_size, args.batch_size):
+                    end = start + args.batch_size
+                    mb_inds = b_inds[start:end]
+
+                    transition = Transition(
+                        state=b_states[mb_inds],
+                        action=b_actions[mb_inds],
+                        log_prob=b_log_probs[mb_inds],
+                        gae_return=b_gae_returns[mb_inds],              
+                        gae_advantage=b_gae_advantages[mb_inds],
+                    )
+
+                    yield global_step, transition
+
 
 
 if __name__ == "__main__":
@@ -219,116 +316,28 @@ if __name__ == "__main__":
         {"params": agent.critic.parameters(), "lr": args.learning_rate, "eps": 1e-5},
     ])
 
-    # ALGO Logic: Storage setup
-    states = torch.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape).to(device)
-    actions = torch.zeros((args.num_steps, args.num_envs) + envs.single_action_space.shape).to(device)
-    logprobs = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    rewards = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    dones = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    values = torch.zeros((args.num_steps, args.num_envs)).to(device)
-
-    # TRY NOT TO MODIFY: start the game
-    global_step = 0
     start_time = time.time()
-    next_obs, _ = envs.reset()
-    next_obs = torch.Tensor(next_obs).to(device)
-    next_done = torch.zeros(args.num_envs).to(device)
-    num_updates = args.total_timesteps // args.buffer_size
-
     ppo = PPO(agent)
+    data_generator = DataGenerator(envs, agent, args)
 
-    for update in range(1, num_updates + 1):
-        for step in range(0, args.num_steps):
-            global_step += args.num_envs
-            states[step] = next_obs
-            dones[step] = next_done
+    for global_step, transition in data_generator:
+        pg_loss, entropy = ppo.actor_loss(transition)
+        v_loss = ppo.critic_loss(transition)
+        entropy_loss = ppo.entropy_loss(entropy)
 
-            # ALGO LOGIC: action logic
-            with torch.no_grad():
-                action, logprob, _, value = agent.get_action_and_value(next_obs)
-                values[step] = value.flatten()
-            actions[step] = action
-            logprobs[step] = logprob
+        loss = pg_loss + entropy_loss + v_loss
 
-            # TRY NOT TO MODIFY: execute the game and log data.
-            next_obs, reward, terminated, truncated, infos = envs.step(action.cpu().numpy())
-            done = terminated | truncated
-            rewards[step] = torch.tensor(reward).to(device).view(-1)
-            next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(done).to(device)
-
-            # Only print when at least 1 env is done
-            if "final_info" not in infos:
-                continue
-
-            for info in infos["final_info"]:
-                # Skip the envs that are not done
-                if info is None:
-                    continue
-                print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
-                writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
-                writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
-
-        # bootstrap value if not done
-        with torch.no_grad():
-            next_value = agent.get_value(next_obs).reshape(1, -1)
-            next_values = torch.cat([values[1:], next_value], dim=0)
-            next_dones = torch.cat([dones[1:], next_done.reshape(1, -1)], dim=0)
-            td_targets = rewards + args.gamma * (1 - next_dones) * next_values
-            td_advantage = td_targets - values
-            gae_advantages = torch.zeros_like(rewards).to(device)
-            gae_advantages = torch.cat([gae_advantages, torch.zeros_like(gae_advantages[:1])], dim=0)
-            for t in reversed(range(args.num_steps)):
-                gae_advantages[t] = td_advantage[t] + args.gamma * args.gae_lambda * (1-next_dones[t]) * gae_advantages[t+1]
-            gae_advantages = gae_advantages[:-1]
-            gae_returns = gae_advantages + values
-
-
-        # flatten the batch
-        b_states = states.reshape((-1,) + envs.single_observation_space.shape)
-        b_values = values.flatten()
-        b_actions = actions.reshape((-1,) + envs.single_action_space.shape)
-        b_rewards = rewards.flatten()
-        b_log_probs = logprobs.flatten()
-        b_gae_advantages = gae_advantages.flatten()
-        b_gae_returns = gae_returns.flatten()
-
-        # Optimizing the policy and value network
-        b_inds = np.arange(args.buffer_size)
-
-        for _ in range(args.buffer_repeat):
-            np.random.shuffle(b_inds)
-            for start in range(0, args.buffer_size, args.batch_size):
-                end = start + args.batch_size
-                mb_inds = b_inds[start:end]
-
-                transition = Transition(
-                    state=b_states[mb_inds],
-                    action=b_actions[mb_inds],
-                    log_prob=b_log_probs[mb_inds],
-                    gae_return=b_gae_returns[mb_inds],              
-                    gae_advantage=b_gae_advantages[mb_inds],
-                )
-
-                pg_loss, entropy = ppo.actor_loss(transition)
-                v_loss = ppo.critic_loss(transition)
-                entropy_loss = ppo.entropy_loss(entropy)
-
-                loss = pg_loss + entropy_loss + v_loss
-
-                optimizer.zero_grad()
-                loss.backward()
-                nn.utils.clip_grad_norm_(agent.parameters(), args.max_grad_norm)
-                optimizer.step()
-
+        optimizer.zero_grad()
+        loss.backward()
+        nn.utils.clip_grad_norm_(agent.parameters(), args.max_grad_norm)
+        optimizer.step()
 
         # TRY NOT TO MODIFY: record rewards for plotting purposes
         writer.add_scalar("charts/learning_rate", optimizer.param_groups[0]["lr"], global_step)
         writer.add_scalar("losses/value_loss", v_loss.item(), global_step)
         writer.add_scalar("losses/policy_loss", pg_loss.item(), global_step)
         writer.add_scalar("losses/entropy", entropy_loss.item(), global_step)
-        writer.add_scalar("charts/advantage", b_gae_advantages.mean().item(), global_step)
-        print("SPS:", int(global_step / (time.time() - start_time)))
-        writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
+        writer.add_scalar("charts/advantage", transition.gae_advantage.mean().item(), global_step)
 
     envs.close()
     writer.close()
